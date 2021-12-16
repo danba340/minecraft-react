93d527470bd80287eb732d185d787ae53bd94ccd
Tommy <tommymaher10@gmail.com>
2021-12-16 22:44:20 +0000

    initial commit:
    removed stateful code spawning blocks.
    
    removed hud

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Physics } from 'use-cannon';
 import { Ground } from './components/Ground';
 import Cubes from './components/Cubes';
 import { Player } from './components/Player';
-import { Hud } from './components/Hud';
+
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
       <Sky sunPosition={[100, 20, 100]} />
       <ambientLight intensity={0.25} />
       <pointLight castShadow intensity={0.7} position={[100, 100, 100]} />
-      <Hud position={[0, 0, -2]} />
+   
       <Physics gravity={[0, -30, 0]}>
         <Ground position={[0, 0.5, 0]} />
         <Player position={[0, 3, 10]} />
--- src/components/Ground.js
+++ src/components/Ground.js
@@ -3,16 +3,13 @@ import { usePlane } from 'use-cannon';
 import { TextureLoader, RepeatWrapping, NearestFilter, LinearMipMapLinearFilter } from 'three';
 
 import grass from '../images/grass.jpg';
-import { useStore } from '../hooks/useStore';
+
 
 export const Ground = (props) => {
   const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }));
   const texture = new TextureLoader().load(grass);
 
-  const [addCube, activeTexture] = useStore((state) => [
-    state.addCube,
-    state.texture,
-  ]);
+ 
   texture.magFilter = NearestFilter;
   texture.minFilter = LinearMipMapLinearFilter;
   texture.wrapS = RepeatWrapping;
@@ -22,13 +19,7 @@ export const Ground = (props) => {
     <mesh
       ref={ref}
       receiveShadow
-      onClick={(e) => {
-        e.stopPropagation();
-        const [x, y, z] = Object.values(e.point).map((coord) =>
-          Math.ceil(coord),
-        );
-        addCube(x, y, z, activeTexture);
-      }}
+  
     >
       <planeBufferGeometry attach="geometry" args={[100, 100]} />
       <meshStandardMaterial map={texture} attach="material" />
