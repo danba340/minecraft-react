aaa3b2dc403e12e24006c3c9c89b985c73dc9378
Tommy <tommymaher10@gmail.com>
2022-01-20 19:15:09 +0000

    implemented button test

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
       <ambientLight intensity={0.25} />
       <pointLight castShadow intensity={0.7} position={[100, 100, 100]} />
       <Physics gravity={[0, -30, 0]}>
-        <Ground position={[0, 0.5, 0]} />
+        <Ground position={[0,0, 0]} />
         <Player position={[0, 3, 10]} />
         <Shape position = {[0, 5, 11]} />
         <UserInterFaceTest  />
--- src/components/UserInterFaceTest.js
+++ src/components/UserInterFaceTest.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import {useBox} from 'use-cannon'
-
-
+import {  Html } from 'drei';
+import "../index.css"
 
 
 export  const UserInterFaceTest = () => {
  
-    const [ref, api] = useBox(() => ({ mass: 1 }))
+    const [ref] = useBox(() => ({ mass: 1.2 }))
     return (
       <mesh ref={ref}  rotation={[0, 0, 0]}>
           <boxBufferGeometry args={[1, 2.5, 2.5]} attach="geometry" />
-        <meshStandardMaterial
+          <meshStandardMaterial
           attach="material"
           color="red"
-        />
+          />
+          <Html position={[0, 0, 0]} distanceFactor={10}        center 
+          
+          >
+          <button className = "button-test"> this is a button
+          </button>
+          </Html>
       </mesh>
     );
   }
